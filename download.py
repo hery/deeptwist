5ba3a37c8bbd1d221831fc570592e5ecab13a0a9
Hery Ratsimihah <hery@careerjet.com>
2019-04-04 12:13:31 +0100

    parser and downloader

---
+++ download.py
@@ -0,0 +1,42 @@
+
+from io import BytesIO
+import json
+from pathlib import Path
+import ntpath
+import requests
+from PIL import Image
+
+
+ASANA = 'bakasana'
+
+
+def download_and_save_image(url):
+    res = requests.get(url)
+    img_content = res.content
+    i = Image.open(BytesIO(img_content))
+    img_name = ntpath.basename(url)
+    query_location = img_name.find('?')
+    img_name = img_name[:query_location]
+    img_path = 'data/%s/%s' % (ASANA, img_name)
+    i.save(img_path)
+
+
+def read_batches():
+    json_path = "data/%s/json/" % ASANA
+    pathlist = Path(json_path).glob('**/*.json')
+    for path in pathlist:
+        path = str(path)
+        data = {}
+        with open(path) as fp:
+            data = json.load(fp)
+            parse_edges(data)
+
+
+def parse_edges(data):
+    for edge in data[:5]:
+        node = edge.get('node')
+        display_url = node.get('display_url')
+        download_and_save_image(display_url)
+
+
+read_batches()
--- parser.py
+++ parser.py
@@ -1,6 +1,16 @@
 
+import ntpath
 import requests
 import json
+
+from io import BytesIO
+from PIL import Image
+
+"""
+   Download JSON data containing image URLs through IG's graphQL API.
+   query_hash, first, and headers need to be set per session.   
+"""
+
 
 url = 'https://www.instagram.com/graphql/query/'
 query_hash = "f92f56d47dc7a55b606908374b43a314"
@@ -19,6 +29,8 @@ headers = {
     "Connection": "keep-alive",
     "Cookie": 'mid=W5DvIAAEAAHAxKrvkh4293IS2CO3; mcd=3; ig_cb=1; fbm_124024574287414=base_domain=.instagram.com; shbid=2882; shbts=1554216199.3265219; csrftoken=qYn9WdLPOHjg47lvNSKdEudpqgXNXDH1; ds_user_id=2204414737; sessionid=2204414737%3AyaYnitXJu1SuVo%3A25; rur=ATN; urlgen="{\"217.138.40.54\": 20952}:1hBih1:LV1DhyLYohrwc60pIA_jjx62A7k'
 }
+
+ASANA = 'bakasana'
 
 def get_edges(after):
     params = {
@@ -40,25 +52,39 @@ def get_edges(after):
         "end_cursor": end_cursor
     }
 
+def write_edge(edge, name='00.json'):
+    with open(name, 'w') as fp:
+        json.dump(edge, fp)
+
 def image_from_edge(edge):
     return edge['node']['display_url']
 
 def get_first_n_batches(n, first=first):
     if n > 0:
         batch = get_edges(first)
+        edge = batch.get('edges')
+        name = 'data/%s/json/%s.json' % (ASANA, n)
+        write_edge(edge, name)
         next_cursor = batch.get('end_cursor')
         get_first_n_batches(n - 1, next_cursor)
-    print('done')
+    else:
+        print('Done')
 
 # first_edges = get_edges(first)
-# first_end_cursor = first_edges.get('end_cursor')
-
-# second_edges = get_edges(first_end_cursor)
-
 # first_edge = first_edges.get('edges')[0]
-# second_edge = second_edges.get('edges')[0]
-# first_image = image_from_edge(first_edge)
-# second_image = image_from_edge(second_edge)
+# first_image_url = image_from_edge(first_edge)
+
+# res = requests.get(first_image_url)
+# image_content = res.content
+# i = Image.open(BytesIO(image_content))
+
+# image_name = ntpath.basename(first_image_url)
+# query_location = image_name.find('?')
+# image_name = image_name[:query_location]
+# image_name = 'data/bakasana/%s' % image_name
+# print(image_name)
+
+# i.save(image_name)
 
 get_first_n_batches(5)
 
